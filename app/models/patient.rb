c951560011bb0651f492ace718ccc64ce34a1238
Ishaan Malhi <contact@ishaanmalhi.me>
2016-12-28 19:24:12 +0530

    feat: Add server side and client side validations.
    Use jQueryvalidation library for client side validations
    and ActiveModel validation helpers for server side validations.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,9 @@
 class Patient < ApplicationRecord
   alias_attribute :phone_no, :mobile
+  validates :email, :presence => true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :mobile, :presence => true,
+                     :numericality => true,
+                     :length => { :minimum => 10, :maximum => 15 }
   after_initialize :set_defaults, unless: :persisted?
   has_many :payments
 
