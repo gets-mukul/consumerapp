621903cbdbe602c5a4b9bc0a30cc156c8ae84a5c
Ranjit <bh.ranjit@gmail.com>
2017-02-08 10:54:44 +0000

    upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Patient.create(name: 'Test User', email: 'test@email.com', mobile: '9000000000')
