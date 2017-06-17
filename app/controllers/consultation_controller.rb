1d5dcc54863bff3b4ba37a85bbda7c8ac917d8be
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-06-17 22:15:59 +0530

    removed the dry skin condition

--- app/controllers/consultation_controller.rb
+++ app/controllers/consultation_controller.rb
@@ -13,12 +13,11 @@ class ConsultationController < ApplicationController
       "Acne" => "https://remedica.typeform.com/to/Eb3Oby",
       "Hairfall or Hair Thinning" => "https://remedica.typeform.com/to/WqEAeB",
       "Pigmentation & Dark Circles" => "https://remedica.typeform.com/to/RgTtE0",
-      "Dry Skin" => "https://remedica.typeform.com/to/RgTtE0",
       "Dandruff" => "https://remedica.typeform.com/to/WqEAeB",
       "Eczema, Psoriasis & Rash" => "https://remedica.typeform.com/to/VuHuwt",
       "Stretch Marks" => "https://remedica.typeform.com/to/lSTMhj",
       "Skin Growth (Moles, Warts)" => "https://remedica.typeform.com/to/qs6Oc7"
-                }
+    }
 
     @condition_form = typeform[@condition] << "?email=#{current_user.email}&mobile=#{current_user.mobile}&name=#{current_user.name}"
     session[:typeform_uid] = typeform[@condition].scan(/\/([\w]*)\?/)[0][0]
