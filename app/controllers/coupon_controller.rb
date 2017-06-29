c9bf091235644ff4bc8a10869ca41295484d9d84
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-06-29 16:32:08 +0530

    added promo=key

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -2,7 +2,7 @@ class CouponController < ApplicationController
   def apply
 	if ['SOCIAL150', 'REFER150'].include? params[:promo_code]
       session[:coupon_applied] = true
-      return redirect_to '/?applied=true&' + params[:promo_code].downcase
+      return redirect_to '/?applied=true&promo=' + params[:promo_code].downcase
     end
     return redirect_to '/'
   end
