362e3d97af812646ee621f07e07f2e652aee1870
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-06-29 16:13:26 +0530

    added multiple coupon and redirected the code to index

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,8 +1,8 @@
 class CouponController < ApplicationController
   def apply
-    if params[:promo_code] == 'DISCOUNT100'
+	if ['SOCIAL100', 'REFER100'].include? params[:promo_code]
       session[:coupon_applied] = true
-      return redirect_to '/?applied=true'
+      return redirect_to '/?applied=true&' + params[:promo_code].downcase
     end
     return redirect_to '/'
   end
