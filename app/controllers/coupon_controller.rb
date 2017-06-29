ea49599fb18a11fb7f76fbb842f5e64db6a43df4
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-06-29 16:18:47 +0530

    changed the coupon code

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,6 +1,6 @@
 class CouponController < ApplicationController
   def apply
-	if ['SOCIAL100', 'REFER100'].include? params[:promo_code]
+	if ['SOCIAL150', 'REFER150'].include? params[:promo_code]
       session[:coupon_applied] = true
       return redirect_to '/?applied=true&' + params[:promo_code].downcase
     end
