b909ee815b620c812e12d41014be57296e22520a
jesse-sravya <jesse.dhara@gmail.com>
2017-08-01 11:45:56 +0530

    added jesse email

--- app/mailers/new_user_notifier_mailer.rb
+++ app/mailers/new_user_notifier_mailer.rb
@@ -2,7 +2,8 @@ class NewUserNotifierMailer < ApplicationMailer
 
   def send_new_user_mail(user)
     @user = user
-    mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "jesse.dhara@gmail.com"],
     :subject => "Remedica: New user sign up." )
   end
 end
