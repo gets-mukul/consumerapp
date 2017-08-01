d2eff1145f49ee269a89d7828c5ab3d089223248
jesse-sravya <jesse.dhara@gmail.com>
2017-08-01 15:06:23 +0530

    referrer

--- app/mailers/new_user_notifier_mailer.rb
+++ app/mailers/new_user_notifier_mailer.rb
@@ -4,7 +4,7 @@ class NewUserNotifierMailer < ApplicationMailer
     @user = user
     # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
     mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "jesse.dhara@gmail.com"],
-    mail( :to => "jesse.dhara@gmail.com",
+    # mail( :to => "jesse.dhara@gmail.com",
     :subject => "Remedica: New user sign up." )
   end
 end
