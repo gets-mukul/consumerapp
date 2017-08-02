10e7bb42d7a60de41d5ad1eb5a045f32325cbe49
jesse-sravya <jesse.dhara@gmail.com>
2017-08-02 12:16:31 +0530

    removed jesse mail

--- app/mailers/new_user_notifier_mailer.rb
+++ app/mailers/new_user_notifier_mailer.rb
@@ -3,8 +3,8 @@ class NewUserNotifierMailer < ApplicationMailer
   def send_new_user_mail(user, referrer)
     @user = user
     @referrer = referrer.downcase.to_s;
-    # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
-    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "jesse.dhara@gmail.com"],
+    mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    # mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "jesse.dhara@gmail.com"],
     # mail( :to => "jesse.dhara@gmail.com",
     :subject => "Remedica: New user sign up." )
   end
