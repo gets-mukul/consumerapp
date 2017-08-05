25bab93a708fce140af00e20c5079d8deead1e7c
jesse-sravya <jesse.dhara@gmail.com>
2017-08-06 02:14:23 +0530

    v4

--- app/mailers/new_user_notifier_mailer.rb
+++ app/mailers/new_user_notifier_mailer.rb
@@ -12,11 +12,11 @@ class NewUserNotifierMailer < ApplicationMailer
   def send_new_user_mail_with_insta(user, referrer, insta, promo_code)
     @user = user
     @referrer = referrer.downcase.to_s;
-    @insta = insta
-    @promo_code = promo_code
-    # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    @insta = insta.to_s
+    @promo_code = promo_code.to_s
+    mail( :to => Rails.application.secrets.ADMIN_EMAIL,
     # mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "jesse.dhara@gmail.com"],
-    mail( :to => "jesse.dhara@gmail.com",
+    # mail( :to => "jesse.dhara@gmail.com",
     :subject => "Remedica: New user sign up." )
   end
 
