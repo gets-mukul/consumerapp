4d38e097abdaa979be325120b7d9844abe7f8ccc
Jesse <jesse.dhara@gmail.com>
2017-09-10 22:59:14 +0530

    changed mail settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://localhost:6379/0', network_timeout: 20}
+  config.redis = {url: 'redis://localhost:6379/0', network_timeout: 25}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://localhost:6379/0', network_timeout: 20}
+  config.redis = {url: 'redis://localhost:6379/0', network_timeout: 25}
 end
