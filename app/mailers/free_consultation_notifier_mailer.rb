aa2269cad54b261babd75261493e3cdab3966d33
Jesse <jesse.dhara@gmail.com>
2017-09-25 15:00:12 +0530

    bug fixes

--- app/mailers/free_consultation_notifier_mailer.rb
+++ app/mailers/free_consultation_notifier_mailer.rb
@@ -1,6 +1,6 @@
 class FreeConsultationNotifierMailer < ApplicationMailer
 
-  def send_user_payment_mail(consultation)
+  def send_free_consultation_notifier_mail(consultation)
     @consultation = consultation
     @coupon = Coupon.find_by_id @consultation.coupon_id
     mail( :to => Rails.application.secrets.ADMIN_EMAIL,
