2b513645be10596558da50fec8cf80d5b6a7d647
Jesse <jesse.dhara@gmail.com>
2017-10-11 16:06:44 +0530

    added le env

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,5 @@ ActionMailer::Base.smtp_settings = {
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.logger = Le.new('bd157c97-dcc9-488d-bfba-31d1cab4e1e7', :debug => true, :local => true)
+
