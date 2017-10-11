a9790f51f1f2737834c91af8ed084b084f1aaa7f
Jesse <jesse.dhara@gmail.com>
2017-10-11 15:35:41 +0530

    removed double migrations

--- db/migrate/20170914123716_add_timestamps_to_payments.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToPayments < ActiveRecord::Migration[5.0]
-  def change
-    add_column :payments, :created_at, :datetime
-    add_column :payments, :updated_at, :datetime
-  end
-end
