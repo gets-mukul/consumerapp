3d3abbde32b129d70d62f8e93ffb37e232392e40
4khil <mail@akhilsingh.net>
2017-12-13 15:02:43 +0000

    selfie 1.2

--- app/models/selfie_form.rb
+++ app/models/selfie_form.rb
@@ -1,11 +1,11 @@
 class SelfieForm < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor, optional: true
+  has_one :selfie_image
   has_and_belongs_to_many :conditions
-  validates_presence_of :patient, :image
+  validates_presence_of :patient
   after_initialize :set_defaults, unless: :persisted?
-  mount_uploader :image, SelfieImageUploader
-    
+
   scope "Created today", -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
   scope "To be diagnosed", -> { where(status: 'pending') }
   scope "Unclear photos", -> { where(status: 'unclear') }
---
+++ test/models/selfie_image_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SelfieImageTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
