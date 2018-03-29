c41b3c0afb926384aa2a23722c553c51190e4313
Ranjit <bh.ranjit@gmail.com>
2018-03-29 05:12:38 +0000

    added headers to FF mails

--- app/mailers/customer_form_filled_mailer.rb
+++ app/mailers/customer_form_filled_mailer.rb
@@ -6,6 +6,8 @@ class CustomerFormFilledMailer < ApplicationMailer
     @skin_hair = (['Hairfall or Hair Thinning', 'Dandruff'].include? @consultation.category) ? 'hair' : 'skin'
     subject = "You’re one click away from amazing " + @skin_hair
     @payment_link = "https://remedicohealth.com/consult/payment/instant_payment?p=" + encrypt(@consultation) + "&utm_source=form_filled&utm_medium=email&utm_campaign=ff_txn_CTA_testimonials_d0_v1"
+    headers['X-SMTPAPI'] = { category: ["form_filled", "ff_txn_CTA_testimonials_d0_v1"] }.to_json
+
     mail( :to => @consultation.patient.email, :subject => subject )
   end
 
@@ -15,6 +17,8 @@ class CustomerFormFilledMailer < ApplicationMailer
     @skin_hair = (['Hairfall or Hair Thinning', 'Dandruff'].include? @consultation.category) ? 'hair' : 'skin'
     subject = "You’re one click away from amazing " + @skin_hair
     @payment_link = "https://remedicohealth.com/consult/payment/instant_payment?p=" + encrypt(@consultation) + "&utm_source=form_filled&utm_medium=email&utm_campaign=ff_txn_locked_plan_testimonials_d0_v1"
+    headers['X-SMTPAPI'] = { category: ["form_filled", "ff_txn_locked_plan_testimonials_d0_v1"] }.to_json
+
     mail( :to => @consultation.patient.email, :subject => subject )
   end
 
@@ -23,6 +27,8 @@ class CustomerFormFilledMailer < ApplicationMailer
     @consultation = consultation
     @skin_hair = (['Hairfall or Hair Thinning', 'Dandruff'].include? @consultation.category) ? 'hair' : 'skin'
     @payment_link = "https://remedicohealth.com/consult/patients/coupon_login?promo=CMAIL50&utm_source=form_filled&utm_medium=email&utm_campaign=ff_discount_locked_plan_satisfaction_d3_v1&url=payment&p=" + encrypt(@consultation)
+    headers['X-SMTPAPI'] = { category: ["form_filled", "ff_discount_locked_plan_satisfaction_d3_v1"] }.to_json
+
     mail( :to => @consultation.patient.email, :subject => "Here’s a gift from us" )
   end
   
@@ -30,6 +36,8 @@ class CustomerFormFilledMailer < ApplicationMailer
   def send_customer_benefits_treatment_plan_mail consultation
     @consultation = consultation
     @payment_link = "https://remedicohealth.com/consult/patients/coupon_login?promo=CMAIL50&utm_source=form_filled&utm_medium=email&utm_campaign=ff_benefits_treatment_plan_d7_v1&url=payment&p=" + encrypt(@consultation)
+    headers['X-SMTPAPI'] = { category: ["form_filled", "ff_benefits_treatment_plan_d7_v1"] }.to_json
+
     mail( :to => @consultation.patient.email, :subject => "What you’ll get in your treatment plan" )
   end
 
@@ -37,6 +45,8 @@ class CustomerFormFilledMailer < ApplicationMailer
   def send_customer_testimonials_mail consultation
     @consultation = consultation
     @payment_link = "https://remedicohealth.com/consult/patients/coupon_login?promo=CMAIL50&utm_source=form_filled&utm_medium=email&utm_campaign=ff_testimonials_d7_v1&url=payment&p=" + encrypt(@consultation)
+    headers['X-SMTPAPI'] = { category: ["form_filled", "ff_testimonials_d7_v1"] }.to_json
+
     mail( :to => @consultation.patient.email, :subject => "Hear from our customers" )
   end
 
