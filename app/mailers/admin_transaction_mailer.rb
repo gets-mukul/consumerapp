917e104c4411efbf082eff0520ad120728ae3a1a
Jesse <jesse.dhara@gmail.com>
2018-06-06 16:14:29 +0530

    selfie checkup flow: minor changes

--- app/mailers/admin_transaction_mailer.rb
+++ app/mailers/admin_transaction_mailer.rb
@@ -18,20 +18,23 @@ class AdminTransactionMailer < ApplicationMailer
     @selfie = SelfieForm.where(:patient => @user).first
     @patient_source = PatientSource.where(:patient => @user).order(:created_at).pluck(:created_at, :local_referrer, :utm_campaign)
 
-    mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "incaishwarya@gmail.com"],
+
     :subject => "Remedico: User payment notice" )
   end
 
   def send_user_reverse_payment_mail my_consultation
     @my_consultation = my_consultation
-    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, 'jesse@remedicohealth.com', 'mail@akhilsingh.net'],
+    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, 'jesse@remedicohealth.com', 'mail@akhilsingh.net', 'incaishwarya@gmail.com'],
     :subject => "Remedico: User payment notice for reverse payment flow" )
   end
 
   def send_free_consultation_notifier_mail consultation
     @consultation = consultation
     @coupon = @consultation.coupon
-    mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    # mail( :to => Rails.application.secrets.ADMIN_EMAIL,
+    mail( :to => [Rails.application.secrets.ADMIN_EMAIL, "incaishwarya@gmail.com"],
     :subject => "Remedico: Free consultation notice." )
   end
 
