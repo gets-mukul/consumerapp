ff6962ddabc90e840ad0e98a47f25e5513518c0f
Jesse <jesse.dhara@gmail.com>
2018-08-17 18:00:01 +0530

    bug fix

--- app/workers/send_out_daily_sms_worker.rb
+++ app/workers/send_out_daily_sms_worker.rb
@@ -17,7 +17,7 @@ class SendOutDailySmsWorker
     # day 3
     selfie_forms = SelfieForm.not_viewed.updated_between(-3, -3)
     selfie_forms.each do |selfie_form|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::HasNotViewed.day_3_sms(selfie_form.patient.name, selfie_form.diagnosis_link),
         mobile: selfie_form.patient.mobile,
         sms_type: 'sms_sc_diagnosis_not_viewed_d3_v1',
@@ -28,7 +28,7 @@ class SendOutDailySmsWorker
     # day 5
     selfie_forms = SelfieForm.not_viewed.updated_between(-5, -5)
     selfie_forms.each do |selfie_form|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::HasNotViewed.day_5_sms(selfie_form.patient.name, selfie_form.diagnosis_link),
         mobile: selfie_form.patient.mobile,
         sms_type: 'sms_sc_diagnosis_not_viewed_d5_v1',
@@ -39,7 +39,7 @@ class SendOutDailySmsWorker
     # day 7
     selfie_forms = SelfieForm.not_viewed.updated_between(-7, -7)
     selfie_forms.each do |selfie_form|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::HasNotViewed.day_7_sms(selfie_form.patient.name, selfie_form.diagnosis_link),
         mobile: selfie_form.patient.mobile,
         sms_type: 'sms_sc_diagnosis_not_viewed_d7_v1',
@@ -53,7 +53,7 @@ class SendOutDailySmsWorker
     # day 3
     patients = Patient.where(:id => SelfieForm.viewed.updated_between(-3, -3).select(:patient_id)).where.not(:id => Consultation.where.not(:user_status => ['form filled', 'free consultation done', 'red flag', 'payment failed', 'paid']).select(:patient_id))
     patients.each do |patient|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::ViewedButNoFF.day_3_sms(patient),
         mobile: patient.mobile,
         sms_type: 'sms_sc_diagnosis_viewed_no_ff_d3_v1',
@@ -64,7 +64,7 @@ class SendOutDailySmsWorker
     # day 7
     patients = Patient.where(:id => SelfieForm.viewed.updated_between(-7, -7).select(:patient_id)).where.not(:id => Consultation.where.not(:user_status => ['form filled', 'free consultation done', 'red flag', 'payment failed', 'paid']).select(:patient_id))
     patients.each do |patient|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::ViewedButNoFF.day_7_sms(patient),
         mobile: patient.mobile,
         sms_type: 'sms_sc_diagnosis_viewed_no_ff_d7_v1',
@@ -78,7 +78,7 @@ class SendOutDailySmsWorker
     # day 3
     consultations = Consultation.where("user_status similar to ?", "form filled|payment failed").where(:patient_id => SelfieForm.viewed.updated_between(-3, -3).select(:patient_id))
     consultations.each do |consultation|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::ViewedButNoFF.day_3_sms(consultation.patient),
         mobile: consultation.patient.mobile,
         sms_type: 'sms_sc_diagnosis_viewed_ff_d3_v1',
@@ -90,7 +90,7 @@ class SendOutDailySmsWorker
     # day 7
     consultations = Consultation.where("user_status similar to ?", "form filled|payment failed").where(:patient_id => SelfieForm.viewed.updated_between(-7, -7).select(:patient_id))
     consultations.each do |consultation|
-      SmsServiceController.sens_sms_new(
+      SmsServiceController.send_sms_new(
         message: SelfieDiagnosis::ViewedButNoFF.day_7_sms(consultation.patient),
         mobile: consultation.patient.mobile,
         sms_type: 'sms_sc_diagnosis_viewed_ff_d7_v1',
